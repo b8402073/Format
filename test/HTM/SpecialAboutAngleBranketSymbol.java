e06c69beeda5381cd1fa2e71df5bff4c09b2b4e6
easterday <b8402073@hotmail.com>
2019-01-23 23:55:58 +0800

    完成HTML的初步修改;  完成UnitTest A01

---
+++ test/HTM/SpecialAboutAngleBranketSymbol.java
@@ -0,0 +1,89 @@
+/*
+  TestJSP933是直接從課本光碟COPY出來的字串
+  所以用舊的作法不能認為CH9的9-33.jsp是完全合法的(JSP/HTML)
+  所以在這裡用腦子創造很多很極端的狀況
+  然後配合(IE/Chrome)的執行結果來校正我們的HTML類別實作
+
+  當然也許有可能用機器產生更完整的狀況會更好!!!
+ */
+package HTM;
+
+import Personal.HTML;
+import Personal.Main;
+import Personal.Pair;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ *
+ * @author easterday
+ */
+public class SpecialAboutAngleBranketSymbol {
+    public String[] jsp=new String[100];
+    public String jsp1="<html><body> < </body></html>";  //遇到游離子要單獨對待    
+    public String jsp2="<html><body> > </body></html>";  //遇到游離子要單獨對待
+    public String jsp3="<html><body> <p  ></body></html>"; //這是一個<p> tag
+    public String jsp4="<html><body> />   </body></html>"; //這是游離子
+    public String jsp5="<html><body> < p ></body></html>"; //這是兩個游離子
+    public String jsp6="<html><body> < p id='>'  > </body></html>"; //這是兩個游離子
+    public String jsp7="<html><body> < p id='<'  > </body></html>"; //這是兩個游離子
+    public String jsp8="<html><body> <p id='>'  > </body></html>"; //這是一個<p> tag
+    public String jsp9="<html><body> <p id='<'  > </body></html>"; //這是<p> tag
+    public String jsp10="<html><body> <p id='>' /> </body></html>"; //這是<p> tag
+    public String jsp11="<html><body> <p id='<'  /> </body></html>"; //這是<p> tag    
+    public String jsp12="<html><body> < p id='name1 \n"+"'></body></html>"; //這裡沒有SQArea,兩個游離子
+    public String jsp13="<html><body> < p id='name1 \n"+"'></body></html>"; //這裡沒有SQArea,兩個游離子
+    public String jsp14="<html><body> <p id='name1 \n"+"'></body></html>"; //這裡沒有SQArea,一個Tag
+    public String jsp15="<html><body> <p id='name1 \n"+"'></body></html>"; //這裡沒有SQArea,一個Tag
+    public String jsp16="<html><body> <p  <!-- GGYY -->   </body></html>"; //一個<p> tag,沒有游離子
+    public String jsp17="<html><body> <p  <!-- GGYY --> > </body></html>"; //一個<p> tag,一個游離子>
+    public String jsp18="<html><body> <p  <!-- GGYY --> /></body></html>"; //一個<p> tag,一個游離子/>
+    public String jsp19="<html><body> <p  <!-- GGYY\n -->   </body></html>"; //一個<p> tag,沒有游離子
+    public String jsp20="<html><body> <p  <!-- GGYY\n --> > </body></html>"; //一個<p> tag,一個游離子>
+    public String jsp21="<html><body> <p  <!-- GGYY\n --> /></body></html>"; //一個<p> tag,一個游離子/>
+    public String jsp22="<html><body><p name=\"GG \n > \" </body></html>";   //一個<p> tag 
+    public String jsp23="<html><body><p name='GG \n > ' </body></html>";   //一個<p> tag 
+    
+    public String f0="<html><body><p </body></html>";  //unfinished tag
+    public String f1="<html><body>< p </body></html>"; //游離子<,  沒有tag
+    public String f2="<html><body><p \n </body></html>"; //unfinished tag
+    public String f3="<html><body><p name='GGYY'  </body></html>"; //unfinished tag
+    public String f4="<html><body><p name=\"GGYY\"   </body></html>"; //unfinished tag
+                
+    public SpecialAboutAngleBranketSymbol() {
+    }
+    
+    @Test
+    public void testA01() {
+        StringBuffer buf=new StringBuffer(jsp1);
+        HTML that=new HTML(buf);
+        that.go();
+        that.PrintIssues();
+        TestHTM1.ReportInTest(that);
+        assertTrue(that.DQArea.size()==0);
+        assertTrue(that.SQArea.size()==0);
+        String[] txtTag={"<html>" ,"<body>" ,"</body>" ,"</html>" };
+        for (int i=0,j=0; i< that.LeftOrRight.size(); i+=2,j++) {
+            int Start= that.LeftOrRight.get(i);
+            int End= that.LeftOrRight.get(i+1);
+            String tmp=Main.ToSTR(new Pair(Start,End), that.MyText);
+            //System.out.println(tmp);
+            //System.out.println(txtTag[j]);
+            assertTrue(txtTag[j].equals(tmp));
+        }
+        for (int j=0,i=1; i<that.LeftOrRight.size()-1; i+=2, j++) {
+            int Start= that.LeftOrRight.get(i);
+            int End=that.LeftOrRight.get(i+1);
+            String tmp=Main.ToSTR(new Pair(Start+1,End-1), that.MyText).trim();
+            //System.out.println("j="+j+" :"+tmp);            
+            switch(j) {
+                case 1:
+                    assertTrue("<".equals(tmp)); break;
+                default:
+                    assertTrue("".equals(tmp));
+            }
+
+        }        
+    }
+    
+}

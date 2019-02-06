481f12ac18ad49526bd855362e258d5e439d140d
easterday <b8402073@hotmail.com>
2019-02-06 22:44:26 +0800

    CompactString_SpecialAboutAngleBranket.java (TestJ8 OK)

--- test/HTM/CompactString_SpecialAboutAngleBranket.java
+++ test/HTM/CompactString_SpecialAboutAngleBranket.java
@@ -291,6 +291,29 @@ public class CompactString_SpecialAboutAngleBranket {
         
         
     }  
+    @Test
+    public void TestJ8() {        
+//    public String jsp8="<html><body> <p id='>'  > </body></html>"; //這是一個<p> tag    
+      StringBuffer buf=new StringBuffer(jsp8);
+        HTML that = new HTML(buf);        
+        that.go();
+        System.out.println("TranslateRadical=TRUE, Finishing=TRUE:");
+        System.out.println(that.toCompactString(true, true));
+        System.out.println("TranslateRadical=TRUE, Finishing=false:");
+        System.out.println(that.toCompactString(true, false));
+        System.out.println("TranslateRadical=false, Finishing=TRUE:");
+        System.out.println(that.toCompactString(false, true));
+        System.out.println("TranslateRadical=false, Finishing=false:");
+        System.out.println(that.toCompactString(false, false));
+        String result1="<html><body><p id='>'></body></html>";
+        
+        assertTrue(result1.equals(that.toCompactString(true, true)));
+        assertTrue(result1.equals(that.toCompactString(true, false)));
+        assertTrue(result1.equals(that.toCompactString(false, true)));
+        assertTrue(result1.equals(that.toCompactString(false, false)));              
+        
+        
+    }  
 
     
 }

c543bfb60eb4bb6f5296a64c062d9bad23a40bf7
easterday <b8402073@hotmail.com>
2019-02-06 22:41:03 +0800

    CompactString_SpecialAboutAngleBranket.java (TestJ7 OK)

--- test/HTM/CompactString_SpecialAboutAngleBranket.java
+++ test/HTM/CompactString_SpecialAboutAngleBranket.java
@@ -247,6 +247,50 @@ public class CompactString_SpecialAboutAngleBranket {
         assertTrue(result2.equals(alt.toCompactString(false, false))); 
         
     }  
+    @Test
+    public void TestJ7() {        
+    //public String jsp7="<html><body> < p id='<'  > </body></html>"; //這是兩個游離子
+      StringBuffer buf=new StringBuffer(jsp7);
+        HTML that = new HTML(buf);        
+        that.go();
+        System.out.println("TranslateRadical=TRUE, Finishing=TRUE:");
+        System.out.println(that.toCompactString(true, true));
+        System.out.println("TranslateRadical=TRUE, Finishing=false:");
+        System.out.println(that.toCompactString(true, false));
+        System.out.println("TranslateRadical=false, Finishing=TRUE:");
+        System.out.println(that.toCompactString(false, true));
+        System.out.println("TranslateRadical=false, Finishing=false:");
+        System.out.println(that.toCompactString(false, false));
+        String result1="<html><body>&lt; p id=&apos;&lt;&apos;  &gt;</body></html>";
+        String result2="<html><body>< p id='<'  ></body></html>";
+        
+        assertTrue(result1.equals(that.toCompactString(true, true)));
+        assertTrue(result1.equals(that.toCompactString(true, false)));
+        assertTrue(result2.equals(that.toCompactString(false, true)));
+        assertTrue(result2.equals(that.toCompactString(false, false)));              
+        
+        System.out.println("ALT:");
+        HTML alt=new HTML(buf);
+        alt.defRadical.add(' ');
+        alt.go();
+        System.out.println("TranslateRadical=TRUE, Finishing=TRUE:");
+        System.out.println(alt.toCompactString(true, true));
+        System.out.println("TranslateRadical=TRUE, Finishing=false:");
+        System.out.println(alt.toCompactString(true, false));
+        System.out.println("TranslateRadical=false, Finishing=TRUE:");
+        System.out.println(alt.toCompactString(false, true));
+        System.out.println("TranslateRadical=false, Finishing=false:");
+        System.out.println(alt.toCompactString(false, false));
+        String result3="<html><body>&lt;&nbsp;p&nbsp;id=&apos;&lt;&apos;&nbsp;&nbsp;&gt;</body></html>";
+        //String result2="<html><body>< p ></body></html>";
+        
+        assertTrue(result3.equals(alt.toCompactString(true, true)));
+        assertTrue(result3.equals(alt.toCompactString(true, false)));
+        assertTrue(result2.equals(alt.toCompactString(false, true)));
+        assertTrue(result2.equals(alt.toCompactString(false, false))); 
+        
+        
+    }  
 
     
 }

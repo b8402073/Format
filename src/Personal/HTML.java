f1804a20e11f74aa0d00ab5ab12744fba8965689
easterday <b8402073@hotmail.com>
2019-02-06 14:24:08 +0800

    try coding with toCompactString

--- src/Personal/HTML.java
+++ src/Personal/HTML.java
@@ -38,6 +38,8 @@ public class HTML {
     public Vector<Integer> Radical;
     //未完成的Tag:[例如<p     ](沒有右括號)
     public Vector<Integer> UnFinished;
+    //設定準備要翻譯的特殊符號  
+    public Vector<Character> defRadical;
 
     public void PrintIssues() {
         System.out.println("DQArea=" + DQArea.toString());
@@ -53,6 +55,8 @@ public class HTML {
     public HTML(StringBuffer inn) {
         MyText = inn;
         Main.refineStringBuffer(MyText);
+        Character[] RadicalSet = {'<', '>', '\'', '"', '&'};
+        setDefRadical(RadicalSet);
     }
 
     public void go() {
@@ -61,6 +65,8 @@ public class HTML {
         CommentArea = new Vector<Pair>();
         Radical = new Vector<Integer>();
         UnFinished = new Vector<Integer>();
+        // defRadical= new Vector<Character>();    
+
         JspStatic3.Build_DQ_Area(MyText, DQArea);
         JspStatic3.Build_SQ_Area(MyText, DQArea, SQArea);
         JspStatic3.Build_Comment_Area(MyText, DQArea, SQArea, CommentArea);
@@ -68,7 +74,7 @@ public class HTML {
         Left = new Vector<Integer>();
         Right = new Vector<Integer>();
         LeftOrRight = new Vector<Integer>();
-        //Build_Left_and_Right_Radical_UnFinished(MyText);
+
         Build();
         for (Integer e : Left) {
             LeftOrRight.add(e);
@@ -124,7 +130,7 @@ public class HTML {
         }
     }
 
-    public int searchForRightAngleBranket1(int start) {
+    private int searchForRightAngleBranket1(int start) {
         for (int i = start; i < MyText.length(); i++) {
             if (Main.In(i, DQArea) || Main.In(i, SQArea)) {
                 continue;
@@ -137,13 +143,13 @@ public class HTML {
         return (-1);
     }
 
-    public int searchForRightAngleBranket2(int start) {
+    private int searchForRightAngleBranket2(int start) {
         for (int i = start; i < MyText.length(); i++) {
             if (Main.In(i, DQArea) || Main.In(i, SQArea) || Main.In(i, CommentArea)) {
                 continue;
             } else {
-                if (MyText.charAt(i) == '%' && MyText.charAt(i+1)=='>') {
-                    return i+1;
+                if (MyText.charAt(i) == '%' && MyText.charAt(i + 1) == '>') {
+                    return i + 1;
                 }
             }
         }
@@ -155,13 +161,15 @@ public class HTML {
         Right.add(e);
     }
 
+    public void setDefRadical(Character[] RadicalSet) {
+        defRadical = new Vector<Character>();
+        for (Character that : RadicalSet) {
+            defRadical.add(that);
+        }
+    }
+
     public void Build() {
         final int StringLength = MyText.length();
-        Character[] RadicalSet = {'<', '>', '\'', '"'};
-        Vector<Character> defRadical = new Vector<Character>();
-        for (Character elem : RadicalSet) {
-            defRadical.add(elem);
-        }
         //boolean flag_in_js = false;        boolean flag_in_jsp = false;        boolean flag_in_pre=false;        boolean flag_in_style=false;
         //用一個stack來代替四五個flag...
         Stack<Focus> stack = new Stack<Focus>();  // Stack for "HTML Comment like <!-- -->"
@@ -192,7 +200,7 @@ public class HTML {
                         if (MyText.substring(e-1,e+1).equals("/>")) {
                             stack.pop();
                         }
-                        */
+                         */
                         StackTagRoutine(i, e);
                         i = e;
                         continue;
@@ -214,11 +222,11 @@ public class HTML {
                         f = new Focus("<%", i, i + 2);
                         stack.push(f);
                         e = searchForRightAngleBranket2(i + 2);
-                        if (e>=0) {
+                        if (e >= 0) {
                             StackTagRoutine(i, e);
                             i = e;
                             stack.pop();
-                        }else { 
+                        } else {
                             Left.add(i);
                             break;
                         }
@@ -261,7 +269,7 @@ public class HTML {
                             //但是這不是我們工作的重點...所以不理它
                             throw new RuntimeException("Unpair: </pre>");
                         }
-                    }                                            
+                    }
                 }
                 if (stack.empty()) {
                     if (i + 1 < StringLength && Character.isWhitespace(MyText.charAt(i + 1))) {
@@ -280,7 +288,7 @@ public class HTML {
             } else if (MyText.charAt(i) == '-') {
                 if (i + 3 < StringLength && "-->".equals(MyText.substring(i, i + 3))) {
                     if (Left.size() - UnFinished.size() - Right.size() == 1) {
-                        if (stack.size() >0 && stack.peek().RetString.equals("<!--")) {
+                        if (stack.size() > 0 && stack.peek().RetString.equals("<!--")) {
                             stack.pop();
                             Right.add(i + 2);
                             i = i + 2;
@@ -295,6 +303,8 @@ public class HTML {
                 }
             } else if (MyText.charAt(i) == '%') {
                 if (i + 2 < StringLength && "%>".equals(MyText.substring(i, i + 2).toLowerCase())) {
+                    //正常來說應該不會跑到這裡來；應該是在發現到"<%"時就處裡掉了
+                    //這一段碼只是放在這裡有備無患....
                     if (stack.size() > 0 && stack.peek().RetString.equals("<%")) {
                         stack.pop();
                         Right.add(i + 1);
@@ -319,169 +329,11 @@ public class HTML {
             }
 
         }
-        if (stack.size() == 1 && stack.peek().RetString.equals("<!--")) {           
+        if (stack.size() == 1 && stack.peek().RetString.equals("<!--")) {
             UnFinished.add(Left.get(Left.size() - 1));
-        }else if (stack.size()==1 &&  stack.peek().RetString.equals("<%")) {
+        } else if (stack.size() == 1 && stack.peek().RetString.equals("<%")) {
             UnFinished.add(Left.get(Left.size() - 1));
         }
-    }
-
-    public void Build_Left_and_Right_Radical_UnFinished(StringBuffer inn) {
-        final int StringLength = inn.length();
-        boolean flag_in_js = false;
-        boolean flag_in_jsp = false;
-        boolean flag_in_htmlComment = false;
-        boolean flag_in_pre = false;
-        boolean flag_in_style = false;
-        for (int i = 0; i < StringLength; i++) {
-            if (inn.substring(i).startsWith("<!--")) {
-                flag_in_htmlComment = true;
-                if (Right.size() < Left.size()) {
-                    UnFinished.add(Left.get(Left.size() - 1));
-                }
-                Left.add(i);
-            } else if (inn.substring(i).startsWith("-->")) {
-                if (Left.size() == Right.size()) {
-                    //這個東西是游離子
-                    Radical.add(i + 2);
-                    i += 2;
-                } else if (Right.size() < Left.size()) {
-                    flag_in_htmlComment = false;
-                    Right.add(i + 2);
-                    i += 2;
-                }
-                continue;
-            }
-
-            if (Main.In(i, DQArea) || Main.In(i, SQArea) || Main.In(i, CommentArea)) {
-                continue;
-            }
-            if (inn.substring(i).startsWith("<%")) {
-                flag_in_jsp = true;
-                if (Right.size() < Left.size()) {
-                    UnFinished.add(Left.get(Left.size() - 1));
-                }
-                Left.add(i);
-            } else if (inn.substring(i).startsWith("%>")) {
-                if (Left.size() == Right.size()) {
-                    //這個東西是游離子
-                    Radical.add(i + 1);
-                    i += 1;
-                } else if (Right.size() < Left.size()) {
-                    flag_in_jsp = false;
-                    Right.add(i + 1);
-                    i += 1;
-                }
-                continue;
-            }
-            String hand = inn.substring(i).toLowerCase();
-            if (hand.startsWith("<script")) {
-                flag_in_js = true;
-                if (Right.size() < Left.size()) {
-                    UnFinished.add(Left.get(Left.size() - 1));
-                }
-                Left.add(i);
-                while (inn.charAt(i) != '>' || Main.In(i, DQArea) || Main.In(i, DQArea)) {
-                    i++;
-                }
-                Right.add(i);
-            } else if (hand.startsWith("</script")) {
-                flag_in_js = false;
-                if (Right.size() < Left.size()) {
-                    UnFinished.add(Left.get(Left.size() - 1));
-                }
-                Left.add(i);
-                while (inn.charAt(i) != '>' || Main.In(i, DQArea) || Main.In(i, SQArea)) {
-                    i++;
-                }
-                Right.add(i);
-                continue;
-            } else if (hand.startsWith("<pre")) {
-                flag_in_pre = true;
-                if (Right.size() < Left.size()) {
-                    UnFinished.add(Left.get(Left.size() - 1));
-                }
-                Left.add(i);
-                while (inn.charAt(i) != '>' || Main.In(i, DQArea) || Main.In(i, SQArea)) {
-                    i++;
-                }
-                Right.add(i);
-                continue;
-            } else if (hand.startsWith("</pre")) {
-                flag_in_pre = false;
-                if (Right.size() < Left.size()) {
-                    UnFinished.add(Left.get(Left.size() - 1));
-                }
-                Left.add(i);
-                while (inn.charAt(i) != '>' || Main.In(i, DQArea) || Main.In(i, SQArea)) {
-                    i++;
-                }
-                Right.add(i);
-                continue;
-            } else if (hand.startsWith("<style")) {
-                flag_in_style = true;
-                if (Right.size() < Left.size()) {
-                    UnFinished.add(Left.get(Left.size() - 1));
-                }
-                Left.add(i);
-                while (inn.charAt(i) != '>' || Main.In(i, DQArea) || Main.In(i, SQArea)) {
-                    i++;
-                }
-                Right.add(i);
-                continue;
-            } else if ((hand.startsWith("</style"))) {
-                flag_in_style = false;
-                if (Right.size() < Left.size()) {
-                    UnFinished.add(Left.get(Left.size() - 1));
-                }
-                Left.add(i);
-                while (inn.charAt(i) != '>' || Main.In(i, DQArea) || Main.In(i, SQArea)) {
-                    i++;
-                }
-                Right.add(i);
-                continue;
-            }
-
-            if (!flag_in_js && !flag_in_jsp && !flag_in_htmlComment
-                    && !flag_in_pre && !flag_in_style) {
-                if (inn.charAt(i) == '<') {
-                    if (i + 1 < StringLength && !Character.isWhitespace(inn.charAt(i + 1))) {
-                        Left.add(i);
-                        for (i += 1; i < StringLength; i++) {
-                            if (i + 1 < StringLength && inn.charAt(i) == '<'
-                                    && !Character.isWhitespace(inn.charAt(i + 1))
-                                    && !Main.In(i, DQArea)
-                                    && !Main.In(i, SQArea)) {
-                                if (Right.size() < Left.size()) {
-                                    UnFinished.add(Left.get(Left.size() - 1));
-                                }
-                                Left.add(i);
-                            }
-                            if (inn.charAt(i) == '>' && !Main.In(i, DQArea) && !Main.In(i, SQArea)) {
-                                Right.add(i);
-                                break;
-                            }
-                        }
-                    } else {
-                        Radical.add(i); //紀錄游離子
-                    }
-                } else if (inn.charAt(i) == '>') { //紀錄游離子
-                    Radical.add(i);
-                } else if (inn.charAt(i) == '\"') {//紀錄游離子
-                    Radical.add(i);
-                } else if (inn.charAt(i) == '\'') {//紀錄游離子
-                    Radical.add(i);
-                }
-
-            }
-        }
-        for (Integer e : Left) {
-            LeftOrRight.add(e);
-        }
-        for (Integer e : Right) {
-            LeftOrRight.add(e);
-        }
-        Collections.sort(LeftOrRight);
     }
 
     public Vector<String> GetAllTags() {
@@ -491,8 +343,8 @@ public class HTML {
             int End = (-1);
             if (i + 1 <= LeftOrRight.size() - 1) {
                 End = LeftOrRight.get(i + 1);
-            }else {
-                End= MyText.length()-1;
+            } else {
+                End = MyText.length() - 1;
             }
 
             if (MyText.charAt(Start) == '<') {
@@ -500,9 +352,9 @@ public class HTML {
                     ret.add(Main.ToSTR(new Pair(Start, End), MyText));
                 } else if (MyText.charAt(End) == '<' && UnFinished.contains(Start)) {
                     ret.add(Main.ToSTR(new Pair(Start, End - 1), MyText));
-                } else if(UnFinished.contains(Start)) {  //位在結尾的UnFinished...
-                    ret.add(Main.ToSTR(new Pair(Start,End), MyText));
-                } 
+                } else if (UnFinished.contains(Start)) {  //位在結尾的UnFinished...
+                    ret.add(Main.ToSTR(new Pair(Start, End), MyText));
+                }
             }
             if (i + 1 == LeftOrRight.size() - 1) {
                 if (MyText.charAt(End) == '>') {
@@ -525,4 +377,77 @@ public class HTML {
         }
         return ret;
     }
+
+    public static String translate(Character input) {
+        switch (input) {
+            case '<':
+                return "&lt;";
+            case '>':
+                return "&gt;";
+            case '\"':
+                return "&quot;";
+            case '\'':
+                return "&apos;";
+            case '&':
+                return "&amp;";
+            default:
+                return "";
+        }
+    }
+
+    public static void InsertToStringBuffer(StringBuffer buf, String Txt, boolean translateRadical) {
+        for (int i = 0; i < Txt.length(); i++) {
+            if (translateRadical) {
+                buf.append(translate(Txt.charAt(i)));
+            } else {
+                buf.append(Txt.charAt(i));
+            }
+        }
+    }
+
+    public String toCompactString(boolean translateRadical, boolean Finishing) {
+        StringBuffer ret = new StringBuffer();
+        if (LeftOrRight.size() > 0) {
+            String startTxt = MyText.substring(0, LeftOrRight.get(0));
+            InsertToStringBuffer(ret, startTxt, translateRadical);
+        }
+        Vector<String> Tag = GetAllTags();
+        Vector<String> Between = GetAllBetween();
+        String that;
+        if (Tag.size() - Between.size() == 1) {
+            for (int i = 0; i < Tag.size(); i++) {
+                that = Tag.get(i);
+                ret.append(that);
+                if (Finishing) {
+                    if (!that.endsWith(">")) {
+                        String append;
+                        if (that.startsWith("<!--")) {
+                            append = "-->";
+                        } else if (that.startsWith("<%")) {
+                            append = "%>";
+                        } else {
+                            append = "/>";
+                        }
+                        ret.append(append);
+                    }
+                }
+                if (i <= Between.size() - 1) {
+                    that = Between.get(i);
+                    InsertToStringBuffer(ret, that, translateRadical);
+                }
+            }
+        }
+        int LastBranket=LeftOrRight.get(LeftOrRight.size()-1);
+        if (LastBranket< MyText.length()) {
+            if (MyText.charAt(LastBranket) == '<') {
+                //do nothing;
+            } else if (MyText.charAt(LastBranket) == '>') {
+                InsertToStringBuffer(ret, MyText.substring(LastBranket+1,MyText.length()),translateRadical  );
+            }                        
+        }
+
+        return ret.toString();
+
+    }
+
 }
